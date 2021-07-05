22fa9f7a1f32f5ab64e4a3ff9a1b205f97ec0069
rgeisreiter <rgeisreiter@yahoo.com>
2021-07-05 17:02:06 -0600

    manager test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,19 +1,19 @@
 const Manager = require("../lib/Manager");
 
 test("Can set office number via constructor argument", () => {
-  const testValue = 100;
-  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  const testValue = 110;
+  const e = new Manager("Ryan", 1, "rgeis@yahoo.com", testValue);
   expect(e.officeNumber).toBe(testValue);
 });
 
 test('getRole() should return "Manager"', () => {
   const testValue = "Manager";
-  const e = new Manager("Foo", 1, "test@test.com", 100);
+  const e = new Manager("Foo", 1, "rgeis@yahoo.com", 110);
   expect(e.getRole()).toBe(testValue);
 });
 
 test("Can get office number via getOffice()", () => {
-  const testValue = 100;
-  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  const testValue = 110;
+  const e = new Manager("Foo", 1, "rgeis@yahoo.com", testValue);
   expect(e.getOfficeNumber()).toBe(testValue);
 });
